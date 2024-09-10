5e24a30b6538ff49bafcfddc542a0935c00cd01d
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-10 16:56:07 +0800

    installed and configured react-router-dom with layout

---
+++ client/src/Layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      <header>Header</header>
+      <Outlet />
+      <footer>Footer</footer>
+    </>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
