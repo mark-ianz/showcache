558464b1bd551b6e1072582430618735094622b4
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-12 22:16:27 +0800

    added a hover underline on movie title

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -9,7 +9,7 @@ export default function MovieCard({ name, image }: MovieCardProps) {
       <img src={image} alt="image" className="rounded-t-xl" />
       <CardContent className="p-4 flex flex-col justify-between flex-grow min-h-40">
         <div className="flex-grow">
-          <h3 className="font-semibold text-lg line-clamp-2 mb-2">{name}</h3>
+          <h3 className="font-semibold text-lg line-clamp-2 mb-2 hover:underline">{name}</h3>
         </div>
         <Button variant={"default"} className="rounded-full w-full">
           Watch Later
