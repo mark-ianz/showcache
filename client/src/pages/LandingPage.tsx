813c8e447bcf07c507490270c5ca5217d39fcd71
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-14 11:27:23 +0800

    added new releases section on landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import { useQuery } from "@tanstack/react-query";
-import { getTrendingThisWeek } from "@/utils/api";
+import { getNewReleases, getTrendingThisWeek } from "@/utils/api";
 
 export type Movie = {
   id: number;
@@ -17,26 +17,53 @@ export type Movie = {
 };
 
 export default function LandingPage() {
-  const { data } = useQuery({
-    queryKey: ["popular_movies"],
+  const { data: trending_this_week } = useQuery({
+    queryKey: ["trending_this_week"],
     queryFn: getTrendingThisWeek,
-    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 5,
   });
 
-  const movies = data?.results;
-  console.log(movies);
+  const { data: new_releases } = useQuery({
+    queryKey: ["new_releases"],
+    queryFn: getNewReleases,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  const newReleases = new_releases?.results;
+  const trending = trending_this_week?.results;
+
   return (
-    <main>
-      <ul className="grid grid-cols-5 gap-4">
-        {movies?.map((movie) => (
-          <li key={movie.id}>
-            <MovieCard
-              name={movie.original_title}
-              image={"https://image.tmdb.org/t/p/original" + movie.poster_path}
-            />
-          </li>
-        ))}
-      </ul>
+    <main className="flex flex-col gap-20">
+      <section>
+        <p className="text-2xl mb-3">Trending This Week</p>
+        <ul className="grid grid-cols-5 gap-4">
+          {trending?.map((movie) => (
+            <li key={movie.id}>
+              <MovieCard
+                name={movie.original_title}
+                image={
+                  "https://image.tmdb.org/t/p/original" + movie.poster_path
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <p className="text-2xl mb-3">New Releases</p>
+        <ul className="grid grid-cols-5 gap-4">
+          {newReleases?.map((movie) => (
+            <li key={movie.id}>
+              <MovieCard
+                name={movie.original_title}
+                image={
+                  "https://image.tmdb.org/t/p/original" + movie.poster_path
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
