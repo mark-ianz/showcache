83a58d2e3d23801d06c0b014a05250e21bc609d2
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-14 19:47:10 +0800

    adjusted the theme switch

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,20 +1,37 @@
-import ThemeSwitch from "../ThemeSwitch";
+import { Settings } from "lucide-react";
+import ThemeSwitch from "../ThemeSwitchButton";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import Logo from "./Logo";
+import { Separator } from "../ui/separator";
+import { useTheme } from "@/theme-provider";
+import ThemeSwitch2 from "../ThemeSwitch2";
 
 export default function Header() {
+  const { theme } = useTheme();
   return (
     <>
       <header className="flex items-center justify-between border-b pb-3 mb-4 w-full">
-        <Logo className={"mr-20"}/>
-        <div className="flex grow gap-4">
+        <Logo className={"mr-20"} />
+        <div className="flex grow gap-4 items-center">
           <Input
             className="max-w-md ml-auto grow"
             placeholder="Search for Movies or TV Shows"
           />
-          <ThemeSwitch />
           <Button>Login</Button>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Settings className="w-8 min-w-6 h-8 min-h-6" />
+            </PopoverTrigger>
+            <PopoverContent align="end" className="mt-2">
+              <p className="text-lg font-semibold">Settings</p>
+              <Separator className="my-2" />
+              <div>
+                <ThemeSwitch2 />
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </header>
     </>
---
+++ client/src/components/ThemeSwitch2.tsx
@@ -0,0 +1,63 @@
+import { Button } from "./ui/button";
+import { useTheme } from "@/theme-provider";
+import { Check } from "lucide-react";
+
+export default function () {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <div>
+      <p>Theme</p>
+      <div className="flex justify-between mt-2">
+        <div className="flex justify-center items-center gap-1">
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            asChild
+            className={`rounded-full border cursor-pointer ${
+              theme === "light" && "border-blue-500"
+            }`}
+            onClick={() => setTheme("light")}
+          >
+            <span className="bg-white w-9 h-9 hover:bg-gray-300">
+              {theme === "light" && <Check color="#3b82f6" />}
+            </span>
+          </Button>
+          <p className="text-sm">Light</p>
+        </div>
+        <div className="flex justify-center items-center gap-1">
+          <Button
+            variant={"outline"}
+            asChild
+            size={"icon"}
+            className={`rounded-full border cursor-pointer ${
+              theme === "dark" && "border-blue-500"
+            }`}
+            onClick={() => setTheme("dark")}
+          >
+            <span className="bg-black w-9 h-9 hover:bg-gray-700">
+              {theme === "dark" && <Check color="#3b82f6" />}
+            </span>
+          </Button>
+          <p className="text-sm">Dark</p>
+        </div>
+        <div className="flex justify-center items-center gap-1">
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            asChild
+            className={`rounded-full border cursor-pointer ${
+              theme === "system" && "border-blue-500"
+            }`}
+            onClick={() => setTheme("system")}
+          >
+            <span className="w-9 h-9 rounded-full bg-[linear-gradient(to_right,black_50%,white_50%)]">
+              {theme === "system" && <Check color="#3b82f6" />}
+            </span>
+          </Button>
+          <p className="text-sm">System</p>
+        </div>
+      </div>
+    </div>
+  );
+}
