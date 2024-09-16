857d8bc7fc9a8b71d92c363376140db1c77639fc
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-16 17:51:58 +0800

    fixed the homepage layout

--- client/src/components/show/ShowListWrapper.tsx
+++ client/src/components/show/ShowListWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function ShowListWrapper({ children }: { children: ReactNode }) {
   return (
-    <ul className="grid grid-cols-8 max-2xl:grid-cols-6 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 gap-4">
+    <ul className="grid gap-4 grid-cols-5 max-xl:grid-cols-4 max-lg:grid-cols-2">
       {children}
     </ul>
   );
