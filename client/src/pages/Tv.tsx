257d178a2c210fb4cfcc498c3afa36860db300a3
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-17 20:03:11 +0800

    refactor tv

--- client/src/pages/Tv.tsx
+++ client/src/pages/Tv.tsx
@@ -3,25 +3,26 @@ import ListMainWrapper from "@/components/ListMainWrapper";
 import ShowSection from "@/components/show/ShowSection";
 import useShows from "@/hooks/useShows";
 import { getTv } from "@/lib/api";
-import { LoaderIcon } from "lucide-react";
 
 export default function Tv() {
   const {
     language: { iso_639_1: language },
   } = useLanguage();
 
-  const { data, error, isError, isLoading } = useShows({
+  const { data, error, isLoading } = useShows({
     queryKey: ["tv", language],
     queryFn: getTv,
   });
 
-  if (!data || isLoading) {
-    return <LoaderIcon />;
-  }
-
   return (
     <ListMainWrapper>
-      <ShowSection showArray={data} title="Top Rated" isTv />
+      <ShowSection
+        showArray={data}
+        error={error}
+        loading={isLoading}
+        title="Top Rated"
+        isTv
+      />
     </ListMainWrapper>
   );
 }
