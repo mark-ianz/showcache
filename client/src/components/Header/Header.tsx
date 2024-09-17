e93f1b84012e4e56510489c73d344428c2b1bc68
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-17 15:17:30 +0800

    added a search selection in searchbar

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,17 +3,38 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import Logo from "./Logo";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import Settings from "./Settings";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { useState } from "react";
 
 export default function Header() {
+  const [searchFor, setSearchFor] = useState<string>("movie");
+
   return (
     <>
       <header className="flex items-center justify-between border-b pb-3 mb-4 w-full">
         <Logo className={"mr-20"} />
         <div className="flex grow gap-4 items-center">
-          <Input
-            className="max-w-md ml-auto grow"
-            placeholder="Search for Movies or TV Shows"
-          />
+          <div className="grow max-w-md ml-auto flex relative">
+            <Input
+              placeholder="Search for Movies or TV Shows"
+              className="pr-20"
+            />
+            <Select value={searchFor} onValueChange={setSearchFor}>
+              <SelectTrigger className="absolute w-fit gap-1 top-1/2 -translate-y-1/2 right-2 text-xs p-1 h-auto border-none focus:ring-0">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent align="end">
+                <SelectItem value="movie">Movie</SelectItem>
+                <SelectItem value="tv">TV Show</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
           <Popover>
             <PopoverTrigger asChild>
               <HamburgerMenuIcon className="w-8 min-w-6 h-8 min-h-6 cursor-pointer" />
