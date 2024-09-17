f2f9990cca9358d4ef092d67afb9bfd55aee462b
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-17 20:02:54 +0800

    refactor newReleases

--- client/src/pages/NewReleases.tsx
+++ client/src/pages/NewReleases.tsx
@@ -3,25 +3,25 @@ import ListMainWrapper from "@/components/ListMainWrapper";
 import ShowSection from "@/components/show/ShowSection";
 import useShows from "@/hooks/useShows";
 import { getNewReleases } from "@/lib/api";
-import { LoaderIcon } from "lucide-react";
 
 export default function NewReleases() {
   const {
     language: { iso_639_1: language },
   } = useLanguage();
 
-  const { data, error, isError, isLoading } = useShows({
+  const { data, error, isLoading } = useShows({
     queryKey: ["new_releases", language],
     queryFn: getNewReleases,
   });
 
-  if (!data || isLoading) {
-    return <LoaderIcon />;
-  }
-
   return (
     <ListMainWrapper>
-      <ShowSection showArray={data} title="New Releases" />
+      <ShowSection
+        showArray={data}
+        error={error}
+        loading={isLoading}
+        title="New Releases"
+      />
     </ListMainWrapper>
   );
 }
