2776364c3320948f6b0ebdf0444faddf1ebac685
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-20 09:55:54 +0800

    adjusted rating text

--- client/src/components/Rating.tsx
+++ client/src/components/Rating.tsx
@@ -1,10 +1,12 @@
 import RatingIcon from "@/icons/RatingIcon";
 
-export default function Rating({ rating }: { rating: number }) {
+export default function Rating({ rating = 0 }: { rating: number }) {
   return (
     <div className="flex items-center gap-1">
       <RatingIcon />
-      <p>{rating.toFixed(1)}</p>
+      <p className="text-sm font-thin text-muted-foreground">
+        {rating?.toFixed(1)}
+      </p>
     </div>
   );
 }
