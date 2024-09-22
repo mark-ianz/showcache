f7c2df6a16236b1458ab20197565fa97a5ce6e34
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-22 21:36:48 +0800

    added backdrops image list

--- client/src/pages/ViewShow.tsx
+++ client/src/pages/ViewShow.tsx
@@ -8,6 +8,9 @@ import { getOneMovie } from "@/api/movies.service";
 import { getCredits, getDirectors } from "@/api/credits.service";
 import { getImages, getTrailers } from "@/api/show.service";
 import { Cast, Crew } from "@/types/credits";
+import Rating from "@/components/Rating";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import { getImg } from "@/lib/helpers";
 
 export default function ViewShow() {
   const { id } = useParams();
@@ -45,9 +48,10 @@ export default function ViewShow() {
     staleTime: 1000 * 60 * 5,
   });
 
-  if (!data || !credits || !trailers || !directors) return <p>loading</p>;
+  if (!data || !credits || !trailers || !directors || !images)
+    return <p>loading</p>;
 
-  console.log(images)
+  console.log(images);
   const genreList = data?.genres.map((genre) => genre.name);
   const directorList = directors.map((director) => director.name);
   const year = data && new Date(data.release_date).getFullYear();
@@ -87,6 +91,25 @@ export default function ViewShow() {
             />
           ))}
         </ScrollableSection>
+
+        <section>
+          <ScrollableSection
+            viewMore={(images.posters.length >= 14) as true} // broken as fuck, I don't know why this shit works
+            viewMoreLink="#"
+            title="Posters"
+          >
+            {images.backdrops.slice(0, 14).map((backdrop, index) => (
+              <li key={backdrop.file_path + index} className="w-[400px]">
+                <AspectRatio ratio={backdrop.aspect_ratio}>
+                  <img
+                    src={getImg(backdrop.file_path, "w780")}
+                    className="w-full h-full object-cover"
+                  />
+                </AspectRatio>
+              </li>
+            ))}
+          </ScrollableSection>
+        </section>
       </main>
     </>
   );
