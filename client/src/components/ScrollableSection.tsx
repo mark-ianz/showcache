21c6eec973bde8e8a8f4dc71555087381498f7b0
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-22 11:44:43 +0800

    removed padding right in ol

--- client/src/components/ScrollableSection.tsx
+++ client/src/components/ScrollableSection.tsx
@@ -21,7 +21,7 @@ export default function ScrollableSection({
       <p className="relative text-3xl font-semibold">{title}</p>
       <ScrollArea className="w-full h-full relative">
         <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-r from-transparent to-background z-10"></div>
-        <ol className="flex gap-3 items-stretch overflow-x-auto py-4 pr-10">
+        <ol className="flex gap-3 items-stretch overflow-x-auto py-4">
           {children}
           <li className="min-w-36 z-10">
             <Card className="h-full">
