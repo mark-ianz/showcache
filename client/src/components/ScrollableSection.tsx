ba63300d24bd842c8dc122b5b66049409beacfc2
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-23 20:35:58 +0800

    headertext

--- client/src/components/ScrollableSection.tsx
+++ client/src/components/ScrollableSection.tsx
@@ -4,6 +4,7 @@ import { ArrowRight } from "lucide-react";
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import HeaderText from "./HeaderText";
 
 type Props = {
   children: ReactNode;
@@ -18,9 +19,10 @@ export default function ScrollableSection({
   viewMoreLink,
   title,
 }: Props) {
+  console.log(viewMore)
   return (
     <div className="mt-10 relative">
-      <p className="relative text-3xl font-semibold">{title}</p>
+      <HeaderText>{title}</HeaderText>
       <ScrollArea className="w-full h-full relative">
         <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-r from-transparent to-background z-10"></div>
         <ol className="flex gap-3 items-stretch overflow-x-auto py-4">
