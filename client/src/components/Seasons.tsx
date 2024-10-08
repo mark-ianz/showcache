fc686c0fe00b1b5b2e6d146b16e35424f2f2dda1
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-08 21:03:02 +0800

    made the seasons wrapper responsive

--- client/src/components/Seasons.tsx
+++ client/src/components/Seasons.tsx
@@ -10,7 +10,7 @@ export default function Seasons({ seasons }: Props) {
   return (
     <section className="gap-2 flex flex-col">
       <HeaderText>Seasons</HeaderText>
-      <ol className="grid grid-cols-6 gap-4">
+      <ol className="grid grid-cols-6 gap-4 max-2xl:grid-cols-5 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2">
         {seasons.map((season) => (
           <ScrollableItem
             key={season.id}
