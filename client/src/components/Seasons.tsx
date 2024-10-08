40dae9062ced9d27f24c4883a0ccd63e9b4593b0
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-08 21:36:01 +0800

    changed seasons layout

--- client/src/components/Seasons.tsx
+++ client/src/components/Seasons.tsx
@@ -1,6 +1,6 @@
 import { Season } from "@/types/tv";
 import HeaderText from "./HeaderText";
-import ScrollableItem from "./ScrollableItem";
+import SeasonCard from "./show/SeasonCard";
 
 type Props = {
   seasons: Season[];
@@ -10,15 +10,9 @@ export default function Seasons({ seasons }: Props) {
   return (
     <section className="gap-2 flex flex-col">
       <HeaderText>Seasons</HeaderText>
-      <ol className="grid grid-cols-6 gap-4 max-2xl:grid-cols-5 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2">
+      <ol className="flex flex-col gap-2">
         {seasons.map((season) => (
-          <ScrollableItem
-            key={season.id}
-            image_path={season.poster_path}
-            path="#"
-            title={`${season.name} (${season.episode_count} eps)`}
-            subtext={season.overview}
-          />
+          <SeasonCard key={season.id} season={season} />
         ))}
       </ol>
     </section>
