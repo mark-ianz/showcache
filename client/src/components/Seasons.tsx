aa7d02191616d33a4ebe9dea9977ee4b21fd04c2
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-08 22:05:48 +0800

    added scrollarea in seasons

--- client/src/components/Seasons.tsx
+++ client/src/components/Seasons.tsx
@@ -1,20 +1,32 @@
 import { Season } from "@/types/tv";
 import HeaderText from "./HeaderText";
 import SeasonCard from "./show/SeasonCard";
+import ScrollableSection from "./ScrollableSection";
+import ScrollableItem from "./ScrollableItem";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { ArrowDown } from "lucide-react";
 
 type Props = {
   seasons: Season[];
 };
 
 export default function Seasons({ seasons }: Props) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   return (
     <section className="gap-2 flex flex-col">
       <HeaderText>Seasons</HeaderText>
-      <ol className="flex flex-col gap-2">
-        {seasons.map((season) => (
-          <SeasonCard key={season.id} season={season} />
-        ))}
-      </ol>
+      <div className="relative">
+        <ScrollArea>
+          <ol className="flex flex-col gap-2">
+            {seasons.map((season) => (
+              <SeasonCard key={season.id} season={season} />
+            ))}
+          </ol>
+        </ScrollArea>
+    
+      </div>
     </section>
   );
 }
