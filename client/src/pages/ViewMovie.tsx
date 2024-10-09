74e3efe0d8399b41aba807163c19acd8e46cb63d
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-09 10:46:32 +0800

    implemented the new casts

--- client/src/pages/ViewMovie.tsx
+++ client/src/pages/ViewMovie.tsx
@@ -1,17 +1,15 @@
-import ScrollableItem from "@/components/ScrollableItem";
 import ScrollableSection from "@/components/ScrollableSection";
 import { useLanguage } from "@/context/language-provider";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getMovieFullDetails } from "@/api/movies.service";
-import { getCredits } from "@/api/credits.service";
 import { getMovieRecommendations } from "@/api/show.service";
-import { Cast, Crew } from "@/types/credits";
 import MediaTabs from "@/components/MediaTabs";
 import { Movie } from "@/types/show";
 import ShowCard from "@/components/show/ShowCard";
 import { cn } from "@/lib/utils";
 import ViewInfoSection from "@/components/show/InfoSection/ViewInfoSection";
+import Casts from "@/components/show/Casts";
 
 export default function ViewMovie() {
   const { id } = useParams();
@@ -25,45 +23,19 @@ export default function ViewMovie() {
     staleTime: 1000 * 60 * 5,
   });
 
-  const { data: credits } = useQuery({
-    queryKey: ["credits", "movie", language, id],
-    queryFn: getCredits,
-    staleTime: 1000 * 60 * 5,
-  });
-
   const { data: recommendations } = useQuery({
     queryKey: ["movie_recommendations", language, id],
     queryFn: getMovieRecommendations,
     staleTime: 1000 * 60 * 5,
   });
 
-  if (!data || !credits || !recommendations) return <p>loading</p>;
-
-  const scrollItems: (Cast | Crew)[] =
-    credits.cast.length > 14
-      ? credits.cast.slice(0, 14)
-      : [...credits.cast, ...credits.crew.slice(0, 14 - credits.cast.length)];
+  if (!data || !recommendations) return <p>loading</p>;
 
   return (
     <>
       <main className="w-full relative flex flex-col gap-10">
         <ViewInfoSection showData={data} />
-
-        <ScrollableSection title="Cast">
-          {scrollItems.map((credit: Cast | Crew, index: number) => (
-            <ScrollableItem
-              path={"/person/" + credits.id}
-              lastItem={index + 1 === scrollItems.length}
-              key={`${credit.id}-${
-                "character" in credit ? credit.character : credit.job
-              }-${index}`}
-              image_path={credit.profile_path}
-              title={credit.name}
-              subtext={"character" in credit ? credit.character : credit.job} // Type narrowing
-            />
-          ))}
-        </ScrollableSection>
-
+        <Casts id={id!} type="movie" />
         <MediaTabs showData={data} />
 
         {recommendations.length > 0 && (
@@ -90,14 +62,4 @@ export default function ViewMovie() {
       </main>
     </>
   );
-}
-
-{
-  /* <ScrollableItem
-  path={"/movie/" + movie.id}
-  lastItem={index + 1 === recommendations.length}
-  key={movie.id}
-  image_path={movie.poster_path}
-  title={movie.title}
-/> */
 }
--- client/src/pages/ViewTv.tsx
+++ client/src/pages/ViewTv.tsx
@@ -1,17 +1,15 @@
-import ScrollableItem from "@/components/ScrollableItem";
 import ScrollableSection from "@/components/ScrollableSection";
 import { useLanguage } from "@/context/language-provider";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-import { getCredits } from "@/api/credits.service";
 import { getTvRecommendations } from "@/api/show.service";
-import { Cast, Crew } from "@/types/credits";
 import MediaTabs from "@/components/MediaTabs";
 import ShowCard from "@/components/show/ShowCard";
 import { getTvFullDetails } from "@/api/tv.service";
 import Seasons from "@/components/Seasons";
 import { cn } from "@/lib/utils";
 import ViewInfoSection from "@/components/show/InfoSection/ViewInfoSection";
+import Casts from "@/components/show/Casts";
 
 export default function ViewTv() {
   const { id } = useParams();
@@ -25,45 +23,19 @@ export default function ViewTv() {
     staleTime: 1000 * 60 * 5,
   });
 
-  const { data: credits } = useQuery({
-    queryKey: ["credits", "tv", language, id],
-    queryFn: getCredits,
-    staleTime: 1000 * 60 * 5,
-  });
-
   const { data: recommendations } = useQuery({
     queryKey: ["tv_recommendations", language, id],
     queryFn: getTvRecommendations,
     staleTime: 1000 * 60 * 5,
   });
 
-  if (!data || !credits || !recommendations) return <p>loading</p>;
-
-  const scrollItems: (Cast | Crew)[] =
-    credits.cast.length > 14
-      ? credits.cast.slice(0, 14)
-      : [...credits.cast, ...credits.crew.slice(0, 14 - credits.cast.length)];
+  if (!data || !recommendations) return <p>loading</p>;
 
   return (
     <>
       <main className="w-full relative flex flex-col gap-10">
         <ViewInfoSection showData={data} />
-
-        <ScrollableSection title="Cast">
-          {scrollItems.map((credit: Cast | Crew, index: number) => (
-            <ScrollableItem
-              path={"/person/" + credits.id}
-              lastItem={index + 1 === scrollItems.length}
-              key={`${credit.id}-${
-                "character" in credit ? credit.character : credit.job
-              }-${index}`}
-              image_path={credit.profile_path}
-              title={credit.name}
-              subtext={"character" in credit ? credit.character : credit.job} // Type narrowing
-            />
-          ))}
-        </ScrollableSection>
-
+        <Casts type="tv" id={id!} />
         <Seasons seasons={data.seasons} />
         <MediaTabs showData={data} />
 
