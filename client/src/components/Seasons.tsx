889ccd61bb11ff824cc4c03985f5125e2b4a16fd
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-09 10:03:46 +0800

    made the seasons list expandable

--- client/src/components/Seasons.tsx
+++ client/src/components/Seasons.tsx
@@ -6,7 +6,8 @@ import ScrollableItem from "./ScrollableItem";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, ArrowDownCircle, ArrowUp } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 type Props = {
   seasons: Season[];
@@ -19,13 +20,37 @@ export default function Seasons({ seasons }: Props) {
       <HeaderText>Seasons</HeaderText>
       <div className="relative">
         <ScrollArea>
-          <ol className="flex flex-col gap-2">
+          <ol
+            className={cn(
+              "flex flex-col gap-2",
+              !isExpanded ? "overflow-hidden max-h-64" : "max-h-[600px]"
+            )}
+          >
             {seasons.map((season) => (
               <SeasonCard key={season.id} season={season} />
             ))}
           </ol>
         </ScrollArea>
-    
+        <div
+          className={cn(
+            "w-full mt-5 flex items-center justify-center",
+            !isExpanded && "bg-gradient-to-b from-transparent to-background absolute bottom-0 h-20 mt-0"
+          )}
+        >
+          <Button
+            size={"icon"}
+            className="rounded-full p-2 border-0 w-12 h-12"
+            variant={"secondary"}
+            asChild
+            onClick={() => setIsExpanded((e) => !e)}
+          >
+            {isExpanded ? (
+              <ArrowUp className="cursor-pointer" />
+            ) : (
+              <ArrowDown className="cursor-pointer" />
+            )}
+          </Button>
+        </div>
       </div>
     </section>
   );
