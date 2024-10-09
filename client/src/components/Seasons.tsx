bf25fcf8cc417076eac8f026af696ef9de81d947
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-09 10:15:21 +0800

    expand all seasons if the there are 3 or less seasons

--- client/src/components/Seasons.tsx
+++ client/src/components/Seasons.tsx
@@ -1,10 +1,10 @@
 import { Season } from "@/types/tv";
 import HeaderText from "./HeaderText";
 import SeasonCard from "./show/SeasonCard";
-import { ScrollArea} from "./ui/scroll-area";
+import { ScrollArea } from "./ui/scroll-area";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { ArrowDown,ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 type Props = {
@@ -13,6 +13,7 @@ type Props = {
 
 export default function Seasons({ seasons }: Props) {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const isFewSeasons = seasons.length > 3;
   return (
     <section className="gap-2 flex flex-col">
       <HeaderText>Seasons</HeaderText>
@@ -21,7 +22,7 @@ export default function Seasons({ seasons }: Props) {
           <ol
             className={cn(
               "flex flex-col gap-2",
-              !isExpanded ? "overflow-hidden max-h-64" : "max-h-[600px]"
+              !isExpanded && isFewSeasons ? "overflow-hidden max-h-64" : "max-h-[650px]"
             )}
           >
             {seasons.map((season) => (
@@ -29,28 +30,34 @@ export default function Seasons({ seasons }: Props) {
             ))}
           </ol>
         </ScrollArea>
-        {!isExpanded && <div className="bg-transparent absolute inset-0"></div>}
-        <div
-          className={cn(
-            "w-full mt-5 flex items-center justify-center",
-            !isExpanded &&
-              "bg-gradient-to-b from-transparent to-background absolute bottom-0 h-20 mt-0"
-          )}
-        >
-          <Button
-            size={"icon"}
-            className="rounded-full p-2 border-0 w-12 h-12"
-            variant={"secondary"}
-            asChild
-            onClick={() => setIsExpanded((e) => !e)}
-          >
-            {isExpanded ? (
-              <ArrowUp className="cursor-pointer" />
-            ) : (
-              <ArrowDown className="cursor-pointer" />
+
+        {/* If few seasons, remove the expandable shits */}
+        {!isExpanded && isFewSeasons && (
+          <div className="bg-transparent absolute inset-0"></div>
+        )}
+        {isFewSeasons && (
+          <div
+            className={cn(
+              "w-full mt-5 flex items-center justify-center",
+              !isExpanded &&
+                "bg-gradient-to-b from-transparent to-background absolute bottom-0 h-20 mt-0"
             )}
-          </Button>
-        </div>
+          >
+            <Button
+              size={"icon"}
+              className="rounded-full p-2 border-0 w-12 h-12"
+              variant={"secondary"}
+              asChild
+              onClick={() => setIsExpanded((e) => !e)}
+            >
+              {isExpanded ? (
+                <ArrowUp className="cursor-pointer" />
+              ) : (
+                <ArrowDown className="cursor-pointer" />
+              )}
+            </Button>
+          </div>
+        )}
       </div>
     </section>
   );
