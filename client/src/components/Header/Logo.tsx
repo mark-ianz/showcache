25a713c3f00d8386d2d40337229e275c862ed474
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-14 21:49:46 +0800

    made the logo as a home link router

--- client/src/components/Header/Logo.tsx
+++ client/src/components/Header/Logo.tsx
@@ -1,12 +1,13 @@
 import { DatabaseIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { ClassNameValue } from "tailwind-merge";
+import { Link } from "react-router-dom";
 
 export default function Logo({ className }: { className?: ClassNameValue }) {
   return (
-    <div className={cn("flex items-center", className)}>
+    <Link to={"/"} className={cn("flex items-center", className)}>
       <h1 className="font-bold text-2xl">Showcache</h1>
       <DatabaseIcon className="" />
-    </div>
+    </Link>
   );
 }
