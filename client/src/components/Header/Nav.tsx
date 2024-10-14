1b7a2027d9afbc25d29dfe117fe3ad377dc33b21
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-14 20:09:13 +0800

    seperated navlinks

--- client/src/components/Header/Nav.tsx
+++ client/src/components/Header/Nav.tsx
@@ -1,21 +1,10 @@
-import { Button } from "../ui/button";
-import { NavLink } from "react-router-dom";
-import { nav_lists } from "@/lib/constants";
+import NavLinks from "./NavLinks";
 
 export default function Nav() {
   return (
     <nav className="w-full mb-[5vh]">
       <ol className="flex justify-around">
-        {nav_lists.map(({ name, path }, index) => (
-          <Button
-            key={index + name}
-            asChild
-            variant={"link"}
-            className="cursor-pointer text-md"
-          >
-            <NavLink to={path}>{name}</NavLink>
-          </Button>
-        ))}
+        <NavLinks />
       </ol>
     </nav>
   );
---
+++ client/src/components/Header/NavLinks.tsx
@@ -0,0 +1,16 @@
+import { nav_lists } from "@/lib/constants";
+import { Button } from "../ui/button";
+import { NavLink } from "react-router-dom";
+
+export default function NavLinks() {
+  return nav_lists.map(({ name, path }, index) => (
+    <Button
+      key={index + name}
+      asChild
+      variant={"link"}
+      className="cursor-pointer text-md"
+    >
+      <NavLink to={path}>{name}</NavLink>
+    </Button>
+  ));
+}
