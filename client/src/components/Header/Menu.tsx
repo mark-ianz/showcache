d11f79d513d5628ff34515343b1b2e7441d4439e
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-14 22:01:18 +0800

    fixed the blinking menu when closing

--- client/src/components/Header/Menu.tsx
+++ client/src/components/Header/Menu.tsx
@@ -6,16 +6,26 @@ import {
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import Settings from "./menu/Settings";
 import MenuNav from "./nav/MenuNav";
+import { useState } from "react";
+import { X } from "lucide-react";
 
 export default function Menu() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <Popover>
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger asChild>
-        <HamburgerMenuIcon className="w-8 min-w-6 h-8 min-h-6 cursor-pointer" />
+        <div className="w-8 min-w-6 h-8 min-h-6 cursor-pointer">
+          {isOpen ? (
+            <X className="w-full h-full" />
+          ) : (
+            <HamburgerMenuIcon className="w-full h-full" />
+          )}
+        </div>
       </PopoverTrigger>
+
       <PopoverContent align="end" className="mt-2 flex flex-col gap-4 text-sm">
         <div className="flex flex-col gap-4">
-          <MenuNav className="hidden max-md:block"/>
+          <MenuNav className="hidden max-md:block" />
           <Settings />
         </div>
       </PopoverContent>
