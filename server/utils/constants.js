5518bd0b74e92576446ba7d309e82d3e1e6ed224
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-16 22:12:21 +0800

    moved all from client folder to main folder & removed the server folder

--- server/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const get_options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: "Bearer " + process.env.TMDB_ACCESS_TOKEN_AUTH,
-  },
-};
--- server/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-export function throwServerError(
-  res,
-  customMessage = "Server Error. Please try again later."
-) {
-  return res.status(500).json({ error: customMessage });
-}
