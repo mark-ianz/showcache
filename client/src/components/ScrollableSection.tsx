c849aa4c8344188980a3bd8f7ac99aa90dbff286
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-16 21:29:34 +0800

    added an olClassName props for ol list

--- client/src/components/ScrollableSection.tsx
+++ client/src/components/ScrollableSection.tsx
@@ -1,22 +1,26 @@
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { ReactNode } from "react";
 import HeaderText from "./HeaderText";
+import { ClassNameValue } from "tailwind-merge";
+import { cn } from "@/lib/utils";
 
 type Props = {
   children: ReactNode;
   title?: string;
+  olClassName?: ClassNameValue;
 };
 
 export default function ScrollableSection({
   children,
   title,
+  olClassName,
 }: Props) {
   return (
     <div>
       {title && <HeaderText className="pb-2">{title}</HeaderText>}
       <ScrollArea className="w-full">
         <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-r from-transparent to-background z-10"></div>
-        <ol className="flex gap-3 items-stretch overflow-x-auto pb-4 justify-start">
+        <ol className={cn("flex gap-3 items-stretch overflow-x-auto pb-4 justify-start", olClassName)}>
           {children}
         </ol>
         <ScrollBar orientation="horizontal" />
