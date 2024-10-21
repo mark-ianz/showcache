b99a68fcfe26080f32a53db80d25d3857aff5822
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-21 22:56:47 +0800

    Seperated the Biography

---
+++ src/components/person/Biography.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+type Props = { biography: string[] | undefined; person: string };
+
+export default function Biography({ biography, person }: Props) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  return (
+    <div className="max-md:text-sm">
+      <p className="text-xl font-semibold mb-2">Biography</p>
+      <div
+        className={cn(
+          "flex flex-col gap-2 text-muted-foreground overflow-hidden relative",
+          !isExpanded && "max-h-40 "
+        )}
+      >
+        {biography && biography.length > 0
+          ? biography?.map((bio, index) => (
+              <p key={index + bio.length}>{bio}</p>
+            ))
+          : "No biography of " + person}
+        {!isExpanded && (
+          <div className="absolute bottom-0 inset-x-0 h-16 bg-gradient-to-b from-transparent to-background"></div>
+        )}
+      </div>
+      <div className="flex justify-end">
+        <p
+          className="text-tertiary underline cursor-pointer"
+          onClick={() => setIsExpanded((current) => !current)}
+        >
+          {!isExpanded ? "See More..." : "See Less..."}
+        </p>
+      </div>
+    </div>
+  );
+}
