5f343a3ff5d32f2cdf1fd73a935843d1446d0bef
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-21 22:42:45 +0800

    created external_ids_logo and external_ids_link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,14 @@
+import fb_icon from "@/assets/fb_icon.png";
+import ig_icon from "@/assets/ig_icon.webp";
+import tiktok_icon from "@/assets/tiktok_icon.webp";
+import twitter_icon from "@/assets/twitter_icon.png";
+import youtube_icon from "@/assets/youtube_icon.png";
+import { ValidExternalIds } from "@/types/credits";
+
 export const nav_lists: { name: string; path: string }[] = [
   {
     name: "Home",
     path: "/",
-
   },
   {
     name: "Top Rated",
@@ -25,3 +31,19 @@ export const nav_lists: { name: string; path: string }[] = [
     path: "/upcoming",
   },
 ];
+
+export const external_ids_logo: ValidExternalIds = {
+  facebook_id: fb_icon,
+  instagram_id: ig_icon,
+  tiktok_id: tiktok_icon,
+  twitter_id: twitter_icon,
+  youtube_id: youtube_icon,
+};
+
+export const external_ids_link: ValidExternalIds = {
+  facebook_id: "https://www.facebook.com/",
+  instagram_id: "https://www.instagram.com/",
+  tiktok_id: "https://www.tiktok.com/@",
+  twitter_id: "https://twitter.com/",
+  youtube_id: "https://www.youtube.com/@",
+};
