e2f204f1c79858f46d4a181ddb13b3370014e746
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-22 18:04:16 +0800

    made the personalinfo values text-muted-foreground

--- src/components/person/PersonalInfo.tsx
+++ src/components/person/PersonalInfo.tsx
@@ -12,31 +12,31 @@ export default function PersonalInfo({ person }: Props) {
       <p className="font-bold text-lg">Personal Info</p>
       <div>
         <p className="font-semibold">Known For</p>
-        <p className="font-sm">{person.known_for_department || "-"}</p>
+        <p className="font-sm text-muted-foreground">{person.known_for_department || "-"}</p>
       </div>
       <div>
         <p className="font-semibold">Gender</p>
-        <p className="font-sm">{getGender(person.gender) || "-"}</p>
+        <p className="font-sm text-muted-foreground">{getGender(person.gender) || "-"}</p>
       </div>
       <div>
         <p className="font-semibold">Birthdate</p>
-        <p className="font-sm">{formatDate(person.birthday) || "-"}</p>
+        <p className="font-sm text-muted-foreground">{formatDate(person.birthday) || "-"}</p>
       </div>
       {person.deathday && (
         <div>
           <p className="font-semibold">Date of Death</p>
-          <p className="font-sm">{formatDate(person.deathday)}</p>
+          <p className="font-sm text-muted-foreground">{formatDate(person.deathday)}</p>
         </div>
       )}
       <div>
         <p className="font-semibold">Place of Birth</p>
-        <p className="font-sm">{person.place_of_birth || "-"}</p>
+        <p className="font-sm text-muted-foreground">{person.place_of_birth || "-"}</p>
       </div>
       <div>
         <p className="font-semibold">Also Known As</p>
         {person.also_known_as.length > 0
           ? person.also_known_as.map((aka, index) => (
-              <p className="font-sm" key={index + aka.length + aka[0]}>
+              <p className="font-sm text-muted-foreground" key={index + aka.length + aka[0]}>
                 {aka}
               </p>
             ))
