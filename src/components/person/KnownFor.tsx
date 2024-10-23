32b9b76ea1e6ad4afcac8164fc412f46050f68c2
[mark-ianz] <bustillomarkian23@gmail.com>
2024-10-23 22:56:22 +0800

    added a query in KnownFor

--- src/components/person/KnownFor.tsx
+++ src/components/person/KnownFor.tsx
@@ -1,7 +1,29 @@
-type Props = {}
+import { getCombinedCredits } from "@/api/show.service";
+import { useLanguage } from "@/context/language-provider";
+import { getShowName } from "@/lib/helpers";
+import { useQuery } from "@tanstack/react-query";
 
-export default function KnownFor({}: Props) {
+type Props = { id: number };
+
+export default function KnownFor({ id }: Props) {
+  const {
+    language: { iso_639_1: language },
+  } = useLanguage();
+
+  const { data } = useQuery({
+    queryKey: ["known_for", language, id],
+    queryFn: getCombinedCredits,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  console.log(data);
   return (
-    <div>KnownFor</div>
-  )
+    <div>
+      <div>
+        {data?.map((show) => (
+          <p key={show.id}>{getShowName(show)}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
