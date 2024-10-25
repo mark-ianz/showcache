4c4ff114755c3ac16d6d5eb6d2bc540794ff8283
mark-ianz <bustillomarkian23@gmail.com>
2024-10-25 21:32:44 +0800

    Created KnownFor layout

--- src/components/person/KnownFor.tsx
+++ src/components/person/KnownFor.tsx
@@ -1,7 +1,11 @@
 import { getCombinedCredits } from "@/api/show.service";
 import { useLanguage } from "@/context/language-provider";
-import { getShowName } from "@/lib/helpers";
+import { getShowName, getShowType } from "@/lib/helpers";
 import { useQuery } from "@tanstack/react-query";
+import ShowCard from "../show/ShowCard";
+import ScrollableSection from "../ScrollableSection";
+import { cn } from "@/lib/utils";
+import HeaderText from "../HeaderText";
 
 type Props = { id: number };
 
@@ -16,14 +20,28 @@ export default function KnownFor({ id }: Props) {
     staleTime: 1000 * 60 * 5,
   });
 
-  console.log(data);
   return (
-    <div>
-      <div>
-        {data?.map((show) => (
-          <p key={show.id}>{getShowName(show)}</p>
+    <>
+      <HeaderText className="text-2xl max-md:text-xl">Known For</HeaderText>
+      <ScrollableSection>
+        {data?.map((show, index) => (
+          <li
+            key={show.id}
+            className={cn(
+              "w-48 max-lg:w-40 max-md:w-36 max-sm:w-32",
+              index + 1 === data.length && "z-10"
+            )}
+          >
+            <ShowCard
+              genre_ids={show.genre_ids}
+              image_path={show.poster_path}
+              name={getShowName(show)}
+              path={`/${getShowType(show)}/${show.id}`}
+              vote_average={show.vote_average}
+            />
+          </li>
         ))}
-      </div>
-    </div>
+      </ScrollableSection>
+    </>
   );
 }
