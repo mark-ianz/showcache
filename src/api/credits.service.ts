c11b6249a8a6e5bf78770108816cab767c2e6a1b
mark-ianz <bustillomarkian23@gmail.com>
2024-10-27 12:11:24 +0800

    added a getPersonTaggedImages and getPersonImages getter fn

--- src/api/credits.service.ts
+++ src/api/credits.service.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { axios_config } from "./axios.config";
 import { CreditsResult, ExternalIds, PersonFullInfo } from "@/types/credits";
 import { QueryFunctionContext } from "@tanstack/react-query";
+import { Image } from "@/types/images";
 
 export async function getDirectors(id: number): Promise<string[]> {
   const { data }: { data: CreditsResult } = await axios.get(
@@ -48,4 +49,41 @@ export async function getPersonExternalIds ({
   );
 
   return data;
+}
+
+export async function getPersonTaggedImages ({
+  queryKey,
+}: QueryFunctionContext): Promise<Image[]> {
+  const [_key, person_id, page = 1] = queryKey;
+  const { data }: { data: TaggedImages } = await axios.get(
+    `https://api.themoviedb.org/3/person/${person_id}/tagged_images`,
+    axios_config({ method: "GET", params: { page }})
+  );
+
+  return data.results;
+}
+
+export async function getPersonImages ({
+  queryKey,
+}: QueryFunctionContext): Promise<Image[]> {
+  const [_key, person_id] = queryKey;
+  const { data }: { data: Images } = await axios.get(
+    `https://api.themoviedb.org/3/person/${person_id}/images`,
+    axios_config({ method: "GET"})
+  );
+
+  return data.profiles;
+}
+
+type Images = {
+  id: number,
+  profiles: Image[]
+}
+
+type TaggedImages = {
+  id: number,
+  page: number,
+  results: Image[],
+  total_pages: number,
+  total_results: number,
 }
