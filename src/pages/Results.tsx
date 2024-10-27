06735676e79ba2a06d8fe8fd19ac8711ef8e96aa
mark-ianz <bustillomarkian23@gmail.com>
2024-10-27 14:08:27 +0800

    added a person result when searching

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,9 +1,13 @@
 import { useLanguage } from "@/context/language-provider";
 import ListMainWrapper from "@/components/ListMainWrapper";
 import ShowSection from "@/components/show/ShowSection";
-import useShows from "@/hooks/useShows";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { getSearchResult } from "@/api/show.service";
+import HeaderText from "@/components/HeaderText";
+import { useQuery } from "@tanstack/react-query";
+import { PersonSearch } from "@/types/credits";
+import { Movie, TV } from "@/types/show";
+import { getImg } from "@/lib/helpers";
 
 export default function Results() {
   const {
@@ -14,20 +18,64 @@ export default function Results() {
   const query = searchParams.get("query");
   const searchFor = searchParams.get("searchFor");
 
-  const { data, error, isLoading } = useShows({
+  const { data, error, isLoading } = useQuery({
     queryKey: ["results", language, query, searchFor],
     queryFn: getSearchResult,
+    staleTime: 1000 * 60 * 60 * 24,
   });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No data</p>;
 
   return (
     <ListMainWrapper>
-      <ShowSection
-        showArray={data}
-        error={error}
-        loading={isLoading}
-        title="Top Rated"
-        isTv={searchFor === "tv"}
-      />
+      {searchFor === "person" ? (
+        <section>
+          <HeaderText className="font-normal mb-2 text-2xl max-md:text-xl">
+            Search Results
+          </HeaderText>
+          <ol className="flex flex-col gap-2">
+            {(data as PersonSearch[])?.map((person) => (
+              <li key={person.id}>
+                <Link to={`/person/${person.id}`} className="flex">
+                  <div className="w-32 h-32 aspect-square">
+                    <img
+                      src={getImg(person.profile_path, "w300", false)}
+                      alt={person.name}
+                      className="object-cover object-center w-full h-full rounded-md"
+                    />
+                  </div>
+                  <div className="pl-2 max-md:text-sm">
+                    <p className="font-semibold">{person.name}</p>
+                    <div className="flex mt-1">
+                      <p>
+                        <span>{person.known_for_department}</span>
+                        <span> • </span>
+                        <span className="font-thin text-muted-foreground">
+                          {person.known_for.map((show, index) => (
+                            <span key={show.id}>
+                              {show.title || show.name}
+                              {person.known_for.length - 1 !== index && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ol>
+        </section>
+      ) : (
+        <ShowSection
+          showArray={data as Movie[] | TV[]}
+          error={error}
+          loading={isLoading}
+          title="Search Results"
+          isTv={searchFor === "tv"}
+        />
+      )}
     </ListMainWrapper>
   );
 }
