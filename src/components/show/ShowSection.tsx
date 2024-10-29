0fa037a51ce433c5181101b2ec315bb12ec1e242
mark-ianz <bustillomarkian23@gmail.com>
2024-10-29 23:41:53 +0800

    fix the loading and error handling, implemented the LoadingAnimation component

--- src/components/show/ShowSection.tsx
+++ src/components/show/ShowSection.tsx
@@ -1,4 +1,5 @@
 import HeaderText from "../HeaderText";
+import LoadingAnimation from "../LoadingAnimation";
 import ShowCard from "./ShowCard";
 import ShowListWrapper from "./ShowListWrapper";
 import { Movie, TV } from "@/types/show";
@@ -19,32 +20,32 @@ export default function ShowSection({
   error,
   loading,
 }: ShowSectionProps) {
-  return showArray ? (
+  if (error) {
+    return <p>There was a server error. Please try again later.</p>;
+  }
+
+  return (
     <section>
       <HeaderText className="font-normal mb-2 text-2xl max-md:text-xl">
         {title}
       </HeaderText>
-      {loading && (
-        <div className="flex items-center justify-center">
-          <LoaderIcon />
-        </div>
+      {loading && <LoadingAnimation />}
+      {showArray && (
+        <ShowListWrapper>
+          {showArray.length === 0 && <p>No result found</p>}
+          {showArray?.map((show) => (
+            <li key={show.id}>
+              <ShowCard
+                path={`/${isTv ? "tv" : "movie"}/${show.id}`}
+                genre_ids={show.genre_ids}
+                vote_average={show.vote_average}
+                name={isTv ? (show as TV).name : (show as Movie).title}
+                image_path={show.poster_path}
+              />
+            </li>
+          ))}
+        </ShowListWrapper>
       )}
-      <ShowListWrapper>
-        {showArray.length === 0 && <p>No result found</p>}
-        {showArray?.map((show) => (
-          <li key={show.id}>
-            <ShowCard
-              path={`/${isTv ? "tv" : "movie"}/${show.id}`}
-              genre_ids={show.genre_ids}
-              vote_average={show.vote_average}
-              name={isTv ? (show as TV).name : (show as Movie).title}
-              image_path={show.poster_path}
-            />
-          </li>
-        ))}
-      </ShowListWrapper>
     </section>
-  ) : (
-    <p>{error?.message}</p>
   );
 }
