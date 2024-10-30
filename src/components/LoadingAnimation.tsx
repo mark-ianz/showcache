8953ba71f373eaf249219bd30a5e90205c492410
mark-ianz <bustillomarkian23@gmail.com>
2024-10-30 12:41:47 +0800

    changed LoadingAnimation

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -1,6 +1,5 @@
 // @ts-nocheck
 import { useTheme } from "@/theme-provider";
-import "ldrs/tailChase";
 import { useEffect, useState } from "react";
 
 type Props = {};
@@ -8,11 +7,7 @@ type Props = {};
 export default function LoadingAnimation({}: Props) {
   return (
     <div className="flex items-center justify-center h-60">
-      <l-tail-chase
-        size="40"
-        speed="1.75"
-        color="rgb(59 130 246)"
-      ></l-tail-chase>
+      <div className="w-10 h-10 border-4 border-t-tertiary border-muted rounded-full animate-spin"></div>
     </div>
   );
 }
