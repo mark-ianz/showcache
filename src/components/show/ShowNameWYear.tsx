96820f513b3f846f5dfcfac8c478de4e4b8dc198
mark-ianz <bustillomarkian23@gmail.com>
2024-10-31 23:45:42 +0800

    reduce the font size, made the year smaller

--- src/components/show/ShowNameWYear.tsx
+++ src/components/show/ShowNameWYear.tsx
@@ -2,9 +2,9 @@ type Props = { year: number; showName: string };
 
 export default function ShowNameWYear({ showName, year }: Props) {
   return (
-    <div className="flex gap-4 items-center text-3xl max-md:text-2xl">
-      <p className="font-bold">{showName}</p>
-      <p>({year || "N/A"})</p>
+    <div className="flex gap-4 items-center">
+      <p className="font-bold text-3xl max-md:text-2xl max-sm:text-xl">{showName}</p>
+      <p className="text-2xl max-md:text-xl max-sm:text-lg">({year || "N/A"})</p>
     </div>
   );
 }
