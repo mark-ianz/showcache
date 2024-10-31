2053956f46b8c2ceeeab2ab527694cb4574d2dfe
mark-ianz <bustillomarkian23@gmail.com>
2024-10-31 23:48:22 +0800

    adjust text size

--- src/components/show/InfoSection/OtherShowDescription.tsx
+++ src/components/show/InfoSection/OtherShowDescription.tsx
@@ -9,7 +9,7 @@ export default function OtherShowDescription({
     <div className="flex flex-col gap-2 max-md:text-sm">
       {tagline && <p className="text-muted-foreground italic">{tagline}</p>}
       <div>
-        <p className="text-xl font-bold">Overview</p>
+        <p className="text-xl font-bold max-md:text-lg">Overview</p>
         <p>{overview || "No overview provided."}</p>
       </div>
       {directorList.length > 0 && (
