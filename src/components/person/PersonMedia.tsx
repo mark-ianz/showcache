9e20182413f5f37bd45f4162d7678289155b597f
mark-ianz <bustillomarkian23@gmail.com>
2024-11-02 22:06:46 +0800

    adjusted the error and loading handling

--- src/components/person/PersonMedia.tsx
+++ src/components/person/PersonMedia.tsx
@@ -1,33 +1,47 @@
 import DynamicMediaTabs from "../DynamicMediaTabs";
 import { useQuery } from "@tanstack/react-query";
 import { getPersonImages, getPersonTaggedImages } from "@/api/credits.service";
+import LoadingAnimation from "../LoadingAnimation";
 
 type Props = {
   id: number;
 };
 
 export default function PersonMedia({ id }: Props) {
-  const { data: tagged_images } = useQuery({
+  const {
+    data: tagged_images,
+    isLoading: tagged_images_isLoading,
+    error: tagged_images_error,
+  } = useQuery({
     queryKey: ["tagged_images", id],
     queryFn: getPersonTaggedImages,
     staleTime: 1000 * 60 * 5,
   });
 
-  const { data: images } = useQuery({
+  const {
+    data: images,
+    isLoading: images_isLoading,
+    error: images_error,
+  } = useQuery({
     queryKey: ["images", id],
     queryFn: getPersonImages,
     staleTime: 1000 * 60 * 5,
   });
 
-  if (!tagged_images || !images) return <p>loading</p>;
-  
+  if (tagged_images_isLoading || images_isLoading) return <LoadingAnimation />;
+  if (tagged_images_error || images_error)
+    return <p>There was a server error. Please try again later.</p>;
+
   return (
-    <DynamicMediaTabs
-      tabs={[
-        { images: images, tab_title: "Images" },
-        { images: tagged_images, tab_title: "Tagged Images" },
-      ]}
-      view_all_link={`/person/${id}/media`}
-    />
+    images &&
+    tagged_images && (
+      <DynamicMediaTabs
+        tabs={[
+          { images: images, tab_title: "Images" },
+          { images: tagged_images, tab_title: "Tagged Images" },
+        ]}
+        view_all_link={`/person/${id}/media`}
+      />
+    )
   );
 }
