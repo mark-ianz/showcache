bd302f4e66a2ad026b9c6b65483bbbcd29cf27b6
mark-ianz <bustillomarkian23@gmail.com>
2024-11-02 22:01:02 +0800

    error and loading handling

--- src/components/show/ShowMedia.tsx
+++ src/components/show/ShowMedia.tsx
@@ -3,6 +3,7 @@ import { getShowImages } from "@/api/show.service";
 import { useQuery } from "@tanstack/react-query";
 import { getShowType } from "@/lib/helpers";
 import { ShowFullDetails } from "@/types/show";
+import LoadingAnimation from "../LoadingAnimation";
 
 type Props = {
   show_data: ShowFullDetails;
@@ -11,21 +12,24 @@ type Props = {
 export default function ShowMedia({ show_data }: Props) {
   const type = getShowType(show_data);
 
-  const { data } = useQuery({
+  const { data, error, isLoading } = useQuery({
     queryKey: ["show_media", show_data.id, type],
     queryFn: getShowImages,
     staleTime: 1000 * 60 * 5,
   });
 
-  if (!data) return <p>loading</p>;
+  if (error) return <p>There was a server error. Please try again later.</p>;
+  if (isLoading) return <LoadingAnimation />;
 
   return (
-    <DynamicMediaTabs
-      tabs={[
-        { images: data?.backdrops, tab_title: "Backdrops" },
-        { images: data?.posters, tab_title: "Posters" },
-      ]}
-      view_all_link={`/show/${show_data.id}/media`}
-    />
+    data && (
+      <DynamicMediaTabs
+        tabs={[
+          { images: data.backdrops, tab_title: "Backdrops" },
+          { images: data.posters, tab_title: "Posters" },
+        ]}
+        view_all_link={`/show/${show_data.id}/media`}
+      />
+    )
   );
 }
