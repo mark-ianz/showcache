47a46f8f667405deea7d4b9e259ae9141396fdba
mark-ianz <bustillomarkian23@gmail.com>
2024-11-02 21:41:45 +0800

    created ShowContent component for show search result

---
+++ src/components/results/ShowContent.tsx
@@ -0,0 +1,27 @@
+import { searchShow } from "@/api/show.service";
+import ShowSection from "../show/ShowSection";
+import { useQuery } from "@tanstack/react-query";
+import { LanguageCode } from "@/types/language";
+import { ShowType } from "@/types/show";
+
+type Props = {
+  query: string;
+  language: LanguageCode;
+  searchFor: ShowType;
+};
+
+export default function ShowContent({ query, language, searchFor }: Props) {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["results", language, query, searchFor],
+    queryFn: searchShow,
+    staleTime: 1000 * 60 * 60,
+  });
+  return (
+    <ShowSection
+      showArray={data}
+      error={error}
+      loading={isLoading}
+      isTv={searchFor === "tv"}
+    />
+  );
+}
