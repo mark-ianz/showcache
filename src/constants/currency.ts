3a6becbcd93fe70f71a57c92c7a8a9fe6c9495ae
mark-ianz <bustillomarkian23@gmail.com>
2024-11-03 13:35:50 +0800

    created currency constant file for conversion

---
+++ src/constants/currency.ts
@@ -0,0 +1,62 @@
+export const currency = [
+  {
+    iso_639_1: "en",
+    locale: "en-US",
+    currency: "USD",
+  },
+  {
+    iso_639_1: "es",
+    locale: "es-ES",
+    currency: "EUR",
+  },
+  {
+    iso_639_1: "fr",
+    locale: "fr-FR",
+    currency: "EUR",
+  },
+  {
+    iso_639_1: "de",
+    locale: "de-DE",
+    currency: "EUR",
+  },
+  {
+    iso_639_1: "zh",
+    locale: "zh-CN",
+    currency: "CNY",
+  },
+  {
+    iso_639_1: "ja",
+    locale: "ja-JP",
+    currency: "JPY",
+  },
+  {
+    iso_639_1: "ko",
+    locale: "ko-KR",
+    currency: "KRW",
+  },
+  {
+    iso_639_1: "ru",
+    locale: "ru-RU",
+    currency: "RUB",
+  },
+  {
+    iso_639_1: "pt",
+    locale: "pt-BR",
+    currency: "BRL",
+  },
+  {
+    iso_639_1: "hi",
+    locale: "hi-IN",
+    currency: "INR",
+  },
+  {
+    iso_639_1: "bn",
+    locale: "bn-BD",
+    currency: "BDT",
+  },
+  {
+    iso_639_1: "ar",
+    locale: "ar-SA",
+    currency: "SAR",
+  },
+];
