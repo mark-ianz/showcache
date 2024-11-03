55dc42f6d0257e457a8b861936f40db8d8a6975a
mark-ianz <bustillomarkian23@gmail.com>
2024-11-03 13:35:32 +0800

    created a formatCurrency fn

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -8,6 +8,10 @@ import { Location } from "react-router-dom";
 import { Movie, ShowFullDetails, ShowType, TV } from "@/types/show";
 import { format } from "date-fns";
 import { ShowCredits } from "@/types/credits";
+import { LanguageCode } from "@/types/language";
+import { currency } from "@/constants/currency";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export function getGenre(genre_ids: number[]): string[] {
   let genre_list: string[] = [];
@@ -109,4 +113,31 @@ export function getShowType(
   show: TvFullDetails | MovieFullDetails | Movie | TV | ShowCredits
 ) {
   return "title" in show ? "movie" : "tv";
+}
+
+export function formatCurrency(value: number, iso_639_1: LanguageCode = "en") {
+  const currencyFormat =
+    currency.find((c) => c.iso_639_1 === iso_639_1) || currency[0];
+  const api_key = import.meta.env.VITE_EXCHANGE_RATE_API_KEY;
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["currency", iso_639_1],
+    queryFn: async () => {
+      const response = await axios.get(
+        `https://v6.exchangerate-api.com/v6/${api_key}/latest/USD`
+      );
+
+      return response.data.conversion_rates;
+    },
+    staleTime: 1000 * 60 * 60 * 24 * 7,
+  });
+
+  if (isLoading) return "Loading...";
+
+  const conversion_rate = data?.[currencyFormat.currency] || 1;
+
+  return new Intl.NumberFormat(currencyFormat?.locale || "en-US", {
+    style: "currency",
+    currency: currencyFormat?.currency || "USD",
+  }).format(Number((value * conversion_rate).toFixed(2)));
 }
