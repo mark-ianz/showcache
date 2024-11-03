0df0f5cb2a922c9ae6ca2917df4c2a4110fefa53
mark-ianz <bustillomarkian23@gmail.com>
2024-11-03 13:36:02 +0800

    added a className props

--- src/components/show/ViewShowLayout.tsx
+++ src/components/show/ViewShowLayout.tsx
@@ -1,9 +1,18 @@
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
+import { ClassNameValue } from "tailwind-merge";
 
-type Props = { children: ReactNode };
+type Props = { children: ReactNode; className?: ClassNameValue };
 
-export default function ViewShowLayout({ children }: Props) {
+export default function ViewShowLayout({ children, className }: Props) {
   return (
-    <main className="w-full relative flex flex-col gap-10 max-md:gap-6 max-sm:gap-4">{children}</main>
+    <main
+      className={cn(
+        "w-full relative flex flex-col gap-10 max-md:gap-6 max-sm:gap-4",
+        className
+      )}
+    >
+      {children}
+    </main>
   );
 }
