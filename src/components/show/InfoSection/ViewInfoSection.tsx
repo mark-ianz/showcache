fe892aae0848760b64d5e8b2d48b8d472416e676
mark-ianz <bustillomarkian23@gmail.com>
2024-11-08 22:07:16 +0800

    wrap the entire component with SectionwBGImage component

--- src/components/show/InfoSection/ViewInfoSection.tsx
+++ src/components/show/InfoSection/ViewInfoSection.tsx
@@ -1,4 +1,4 @@
-import { getImg, getShowDuration } from "@/lib/helpers";
+import { getShowDuration } from "@/lib/helpers";
 import JoinGenreList from "./JoinGenreList";
 import Rating from "@/components/Rating";
 import ShowNameWYear from "../ShowNameWYear";
@@ -7,34 +7,20 @@ import OptionButtons from "./OptionButtons";
 import ShowPoster from "./ShowPoster";
 import { ShowFullDetails } from "@/types/show";
 import ViewImage from "@/components/ViewImage";
+import SectionwBGImage from "../SectionwBGImage";
 
 type Props = {
   showData: ShowFullDetails;
 };
 
 export default function ViewInfoSection({ showData }: Props) {
-  const backdrop_path = getImg({
-    path: showData.backdrop_path,
-    size: "original",
-    mediaType: "show",
-    undefineable: true,
-  });
-
   return (
-    <section
-      className="p-6 relative"
-      style={{
-        backgroundImage: `url(${backdrop_path})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <SectionwBGImage backdrop_path={showData.backdrop_path}>
       {/* Overlay */}
-      <div className="absolute inset-0 bg-background opacity-90 z-0"></div>
       <div className="relative z-10 items-center">
         <div className="flex gap-10 max-md:flex-col items-center">
           <ViewImage src={showData.poster_path} mediaType="show">
-            <ShowPoster showData={showData} />
+            <ShowPoster poster_path={showData.poster_path} />
           </ViewImage>
           <div className="flex flex-col gap-4">
             <div className="flex gap-4 font-thin items-center max-md:text-sm max-sm:text-xs">
@@ -48,6 +34,6 @@ export default function ViewInfoSection({ showData }: Props) {
           </div>
         </div>
       </div>
-    </section>
+    </SectionwBGImage>
   );
 }
