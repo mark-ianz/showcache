f526269d9cb537c7e186331b821d1936b56d5990
mark-ianz <bustillomarkian23@gmail.com>
2024-11-08 22:06:36 +0800

    changed the props from showData to just poster_path only

--- src/components/show/InfoSection/ShowPoster.tsx
+++ src/components/show/InfoSection/ShowPoster.tsx
@@ -1,17 +1,15 @@
-import { getImg, getShowName } from "@/lib/helpers";
-import { ShowFullDetails } from "@/types/show";
+import { getImg } from "@/lib/helpers";
 
-type Props = { showData: ShowFullDetails };
+type Props = { poster_path: string };
 
-export default function ShowPoster({ showData }: Props) {
-  const poster_path = getImg({path: showData.poster_path, size: "w780", mediaType: "show"});
+export default function ShowPoster({ poster_path }: Props) {
+  const poster_path_src = getImg({path: poster_path, size: "w780", mediaType: "show"});
 
   return (
     <div className="w-72 max-xsm:w-56">
-      {poster_path && (
+      {poster_path_src && (
         <img
-          src={poster_path}
-          alt={getShowName(showData) + " poster path"}
+          src={poster_path_src}
           className="object-cover rounded-lg min-w-60 max-md:min-w-0"
         />
       )}
