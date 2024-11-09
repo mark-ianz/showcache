57ab3140e041738e2c9218e8ed669e328da733a5
mark-ianz <bustillomarkian23@gmail.com>
2024-11-09 14:52:31 +0800

    created Collection Media component

---
+++ src/components/show/CollectionMedia.tsx
@@ -0,0 +1,26 @@
+import { getCollectionImages } from "@/api/movies.service";
+import { useQuery } from "@tanstack/react-query";
+import DynamicMediaTabs from "../DynamicMediaTabs";
+
+type Props = { id: number };
+
+export default function CollectionMedia({ id }: Props) {
+  const { data } = useQuery({
+    queryKey: ["collection_media", id],
+    queryFn: getCollectionImages,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  console.log(data);
+  return (
+    data && (
+      <DynamicMediaTabs
+        tabs={[
+          { images: data.backdrops, tab_title: "Backdrops" },
+          { images: data.posters, tab_title: "Posters" },
+        ]}
+        view_all_link={`/collection/${id}/media`}
+      />
+    )
+  );
+}
