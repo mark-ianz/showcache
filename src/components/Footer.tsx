4f63c59606c0e51c9c4c03cbfc81c66b5894e003
mark-ianz <bustillomarkian23@gmail.com>
2024-11-12 13:43:07 +0800

    created a Footer component with links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+export default function Footer() {
+  const links = [
+    {
+      name: "About",
+      url: "/about",
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+    },
+    {
+      name: "Privacy Policy",
+      url: "/privacy",
+    },
+  ];
+
+  return (
+    <footer className="py-6">
+      <div className="container mx-auto flex flex-col items-center">
+        <div className="flex gap-4">
+          {links.map((link) => (
+            <Button variant={"link"} className="p-0 text-muted-foreground" asChild key={link.name}>
+              <Link to={link.url}>{link.name}</Link>
+            </Button>
+          ))}
+        </div>
+        <div className="flex justify-center">
+          <p className="text-sm text-muted-foreground">
+            &copy; {new Date().getFullYear()} ShowCache. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
