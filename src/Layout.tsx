93ad4998d3dfed58e0bb88343ad47f1e41fa870f
mark-ianz <bustillomarkian23@gmail.com>
2024-11-12 13:59:24 +0800

    added a child props in Layout component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,14 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import Nav from "./components/header/nav/Nav";
 import Footer from "./components/Footer";
+import { ReactNode } from "react";
 
-export default function Layout() {
+export default function Layout({children}: {children?: ReactNode}) {
   return (
     <div className="wrapper py-4 px-40 flex flex-col items-center justify-center max-2xl:px-[8vw] max-lg:px-[4vw]">
       <>
         <Header />
         <Nav />
         <Outlet />
+        {children}
         <Footer />
       </>
     </div>
