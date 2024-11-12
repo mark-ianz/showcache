1646469fb8d7e5f4598a06d33e6edb5365728981
mark-ianz <bustillomarkian23@gmail.com>
2024-11-12 13:43:15 +0800

    added the CustomFooter in the Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import Nav from "./components/header/nav/Nav";
+import Footer from "./components/Footer";
 
 export default function Layout() {
   return (
@@ -9,6 +10,7 @@ export default function Layout() {
         <Header />
         <Nav />
         <Outlet />
+        <Footer />
       </>
     </div>
   );
