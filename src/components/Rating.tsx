9e801ea0cb4a3ee54f77df29dd18cd7ec0e23a41
mark-ianz <bustillomarkian23@gmail.com>
2024-11-13 21:18:33 +0800

    make the render conditional, only renders if rating is not 0

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,12 +1,14 @@
 import RatingIcon from "@/icons/RatingIcon";
 
-export default function Rating({ rating = 0 }: { rating: number }) {
+export default function Rating({ rating }: { rating: number }) {
   return (
-    <div className="flex items-center gap-1">
-      <RatingIcon />
-      <p className="text-sm font-thin text-muted-foreground max-sm:text-xs">
-        {rating?.toFixed(1)}
-      </p>
-    </div>
+    rating !== 0 && (
+      <div className="flex items-center gap-1">
+        <RatingIcon />
+        <p className="text-sm font-thin text-muted-foreground max-sm:text-xs">
+          {rating.toFixed(1)}
+        </p>
+      </div>
+    )
   );
 }
