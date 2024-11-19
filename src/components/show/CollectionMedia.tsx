a669e0e399b4ad48b9f4240874853bf85736f64d
mark-ianz <bustillomarkian23@gmail.com>
2024-11-19 11:55:25 +0800

    added a collectionName props and extended the path for view_all_link

--- src/components/show/CollectionMedia.tsx
+++ src/components/show/CollectionMedia.tsx
@@ -2,23 +2,23 @@ import { getCollectionImages } from "@/api/movies.service";
 import { useQuery } from "@tanstack/react-query";
 import DynamicMediaTabs from "../DynamicMediaTabs";
 
-type Props = { id: number };
+type Props = { id: number; collectionName: string };
 
-export default function CollectionMedia({ id }: Props) {
-  const { data } = useQuery({
+export default function CollectionMedia({ id, collectionName }: Props) {
+  const { data: images } = useQuery({
     queryKey: ["collection_media", id],
     queryFn: getCollectionImages,
     staleTime: 1000 * 60 * 5,
   });
 
   return (
-    data && (
+    images && (
       <DynamicMediaTabs
         tabs={[
-          { images: data.backdrops, tab_title: "Backdrops" },
-          { images: data.posters, tab_title: "Posters" },
+          { images: images.backdrops, tab_title: "Backdrops" },
+          { images: images.posters, tab_title: "Posters" },
         ]}
-        view_all_link={`/media/collection/${id}`}
+        view_all_link={`/media/collection/${id}/${collectionName}`}
       />
     )
   );
