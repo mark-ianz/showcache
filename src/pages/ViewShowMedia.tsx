ab3651bd85b7ce6a1ec70f338fe6c36e16dbe8bd
mark-ianz <bustillomarkian23@gmail.com>
2024-11-19 12:00:04 +0800

    refactored the ViewShowMedia

--- src/pages/ViewShowMedia.tsx
+++ src/pages/ViewShowMedia.tsx
@@ -1,60 +1,14 @@
 import { getShowImages } from "@/api/show.service";
 import ErrorComponent from "@/components/ErrorComponent";
-import HeaderText from "@/components/HeaderText";
 import LoadingAnimation from "@/components/LoadingAnimation";
-import Rating from "@/components/Rating";
-import ViewImage from "@/components/ViewImage";
-import { getImg } from "@/lib/helpers";
-import { cn } from "@/lib/utils";
-import { Image } from "@/types/images";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-
-function MediaList({ images }: { images: Image[] }) {
-  const mediaOrientation =
-    images[0].aspect_ratio > 1.5 ? "landscape" : "portrait";
-
-  return (
-    <ol className={cn("grid gap-4 grid-cols-3 max-xl:grid-cols-2 max-md:grid-cols-1 w-full")}>
-      {images.map((image) => (
-        <ViewImage
-          src={image.file_path}
-          mediaType="show"
-          mediaOrientation={mediaOrientation}
-        >
-          <li className={cn("border shadow-md rounded-md text-sm grow")}>
-            <div>
-              <img
-                src={getImg({
-                  path: image.file_path,
-                  size: "w780",
-                  mediaType: "show",
-                })}
-                className="w-full h-full object-cover object-center rounded-t-md"
-              />
-            </div>
-            <div className="p-2 relative flex flex-col gap-2 border-t max-md:gap-1">
-              <div className="flex items-start absolute right-2">
-                <Rating rating={image.vote_average} />
-              </div>
-              <div className="flex flex-col items-start">
-                <p className="text-xs">Size</p>
-                <p>{image.width + "x" + image.height}</p>
-              </div>
-              <div className="flex flex-col items-start">
-                <p className="text-xs">Aspect Ratio</p>
-                <p>{image.aspect_ratio}</p>
-              </div>
-            </div>
-          </li>
-        </ViewImage>
-      ))}
-    </ol>
-  );
-}
+import TitleAndOptions from "@/components/view_all_media/TitleAndOptions";
+import ListContainer from "@/components/view_all_media/ListContainer";
+import MediaListContainer from "@/components/view_all_media/MediaListContainer";
 
 export default function ViewShowMedia() {
-  const { type, id } = useParams();
+  const { type, id, title } = useParams();
 
   const {
     data: images,
@@ -72,17 +26,11 @@ export default function ViewShowMedia() {
   return (
     images && (
       <main className="w-full">
-        <HeaderText>Media</HeaderText>
-        <div className="flex gap-10 justify-between max-lg:gap-6 max-md:gap-4">
-          <section className="grow">
-            <HeaderText className="mb-2">Backdrops</HeaderText>
-            <MediaList images={images.backdrops} />
-          </section>
-          <section className="grow">
-            <HeaderText className="mb-2">Posters</HeaderText>
-            <MediaList images={images.posters} />
-          </section>
-        </div>
+        <TitleAndOptions title={title!} />
+        <ListContainer>
+          <MediaListContainer image={images.backdrops} title="Backdrops"/>
+          <MediaListContainer image={images.posters} title="Posters" />
+        </ListContainer>
       </main>
     )
   );
