c87e19cfecfe42c3451cca5cbaaeeeaf9fe60e17
mark-ianz <bustillomarkian23@gmail.com>
2024-11-27 14:47:15 +0800

    modify the color of button

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -24,10 +24,10 @@ export default function ScrollToTopButton() {
     isVisible && (
       <Button
         size={"icon"}
-        className="rounded-full fixed bottom-10 right-10 w-12 h-12 z-50 max-md:bottom-6 max-md:right-6"
+        className="rounded-full bg-muted-foreground fixed bottom-10 right-10 w-12 h-12 z-50 max-md:bottom-6 max-md:right-6"
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       >
-        <ArrowUpIcon className="text-muted-foreground w-7 h-7" />
+        <ArrowUpIcon className="text-background w-7 h-7" />
       </Button>
     )
   );
