c99f23292fa5276a4c43050db49edacf4c6aae0f
mark-ianz <bustillomarkian23@gmail.com>
2024-11-27 14:54:10 +0800

    commented the filters and sort on view all media. tinatamad na ako tapusin tangena

--- src/components/view_all_media/TitleAndOptions.tsx
+++ src/components/view_all_media/TitleAndOptions.tsx
@@ -1,7 +1,7 @@
 import HeaderText from "../HeaderText";
-import { Button } from "../ui/button";
+/* import { Button } from "../ui/button";
 import { CaretSortIcon } from "@radix-ui/react-icons";
-import { Filter } from "lucide-react";
+import { Filter } from "lucide-react"; */
 import { ClassNameValue } from "tailwind-merge";
 import { cn } from "@/lib/utils";
 
@@ -11,7 +11,7 @@ export default function TitleAndOptions({ title, className }: Props) {
   return (
     <div className={cn(className, "mb-6 max-xl:mb-4 max-lg:mb-2")}>
       <HeaderText>{`All Media for ${title}`}</HeaderText>
-      <div className="flex items-center mt-2">
+      {/* <div className="flex items-center mt-2">
         <Button variant={"ghost"} className="flex items-center gap-1 p-2">
           <p>Sort By</p>
           <CaretSortIcon className="w-5 h-5" />
@@ -20,7 +20,7 @@ export default function TitleAndOptions({ title, className }: Props) {
           <p>Filter</p>
           <Filter className="w-5 h-5" />
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 }
